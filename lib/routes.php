6fae733a21ecec1f3c99c8096d31234b42173ef9
Anoop <anoop.srivastava@laitkor.com>
2012-10-17 16:12:00 +0530

    Added the constant, routes file

---
+++ lib/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/********************************************
+*	Author 	:	Anoop Kumar Srivastava		*
+*	Date	:	17 Oct 2012					*
+********************************************/
+
+$pq_routes[] = array('all-products','products','pq_showall');
+$pq_routes[] = array('cart','cart','pq_cart');
+$pq_routes[] = array('checkout','cart','pq_checkout');
+
+$custom_urls = apply_filters('pq_routes', 'slug','controller', 'function');
+
+$pq_routes = array_merge($pq_routes, $custom_urls);
+
+$url_param = parse_url(site_url().$_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+if(isset($_REQUEST['pq'])){
+	foreach($pq_routes as $rurl){
+		if($_REQUEST['pq'] == $rurl[0]){			
+			if(!function_exists($rurl[2]))
+				include PQ_ROOT_PATH.$rurl[1].'.php';
+			parse_str($url_param['query'], $pass_arg);
+			call_user_func($rurl[2],$pass_arg);		
+		}
+	}
+}
+?>
--- product_quote.php
+++ product_quote.php
@@ -2,10 +2,36 @@
 /*
 Plugin Name: Get Products Quote
 Plugin URI: http://anoop4u.wordpress.com/get-product-quote
-Description: Lat the visoty to get a price quote for various products
+Description: Let the visoty to get a price quote for various products
 Version: 1.0
 Author: Anoop Kumar Srivastava
 Author URI: http://anoop4u.wordpress.com/
 License: GPL2
 */
+/*
+Products Quote - Let the visoty to get a price quote for various products
+Copyright (C) 2012  Anoop Srivastava
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+if(!defined('PQ_ROOT_PATH')){
+		define('PQ_ROOT_PATH', plugin_dir_path(__FILE__).'/');
+		define('PQ_URL',plugins_url());
+}
+// Adding all the required file
+require(PQ_ROOT_PATH."lib/constant.php");
+require(PQ_ROOT_PATH."lib/utility.php");	
+require(PQ_ROOT_PATH."lib/routes.php");	
 ?>
