801e1e9f7add39701295ce399bba6116d18197fa
Anoop <anoop.srivastava@laitkor.com>
2012-10-17 14:43:46 +0530

    First commit

---
+++ product_quote.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Get Products Quote
+Plugin URI: http://anoop4u.wordpress.com/get-product-quote
+Description: Lat the visoty to get a price quote for various products
+Version: 1.0
+Author: Anoop Kumar Srivastava
+Author URI: http://anoop4u.wordpress.com/
+License: GPL2
+*/
+?>
